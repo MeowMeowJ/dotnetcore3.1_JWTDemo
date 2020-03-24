b29eafccc5f4d8e849ddf76df79e06d8b919fe0c
MeowMeowJ <641911805@qq.com>
2020-03-24 22:24:44 +0800

    JwtBearer

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jwt.Controllers
+{
+    [Route("{controller}")]
+    [ApiController]
+    public class LoginController: ControllerBase
+    {
+        public string GetToken()
+        {
+            // 3 + 2
+            SecurityToken securityToken = new JwtSecurityToken(
+                // 搭建人
+                issuer: "issuer",
+                // 订阅人
+                audience: "audience",
+                // 验证令牌
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.ASCII.GetBytes("laukitlaukitlaukit")),
+                    SecurityAlgorithms.HmacSha256),
+
+                // 过期日期
+                expires: DateTime.Now.AddHours(1),
+                // 声明
+                claims: new Claim[] {}
+            );
+       
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Jwt
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            SecurityKey securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("laukitlaukitlaukit"));
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        // 3 + 2
+                        
+                        // 验证密钥
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = securityKey,
+
+                        // 验证搭建人
+                        ValidateIssuer = true,
+                        ValidIssuer = "issuer", // 字符串可以随便写，但要和后面一致
+
+                        // 验证订阅人
+                        ValidateAudience = true,
+                        ValidAudience = "audience",
+
+                        // 需要验证过期时间
+                        RequireExpirationTime = true,
+                        // 生命周期
+                        ValidateLifetime = true,
+                    };
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication(); // 开启验证
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
